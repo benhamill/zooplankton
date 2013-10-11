16b02a1c924948e692ffe84e3197c37e4db1a6b7
Ben Hamill <git-commits@benhamill.com>
2013-10-11 00:23:02 -0500

    Some specs for what should be working now.

--- lib/zooplankton.rb
+++ lib/zooplankton.rb
@@ -1,4 +1,47 @@
 require "zooplankton/version"
 
 module Zooplankton
+  class << self
+    def path_template_for(helper_name, params={})
+      return unless named_routes.names.include?(helper_name)
+
+      unescape_template(expand_helper(helper_name, :path, params))
+    end
+
+    def url_template_for(helper_name, params={})
+      return unless named_routes.names.include?(helper_name)
+
+      unescape_template(expand_helper(helper_name, :url, params))
+    end
+
+    private
+
+    def expand_helper(helper_name, path_or_url, params)
+      helper_method = "#{helper_name}_#{path_or_url}"
+
+      url_helpers.send(helper_method, *templated_required_params_for(helper_name, params))
+    end
+
+    def named_routes
+      Rails.application.routes.named_routes
+    end
+
+    def route_object_for(helper_name)
+      named_routes.routes[helper_name]
+    end
+
+    def templated_required_params_for(helper_name, params)
+      route_object_for(helper_name).required_parts.map do |required_part|
+        params.fetch(required_part) { "{#{required_part}}" }
+      end
+    end
+
+    def unescape_template(template)
+      CGI.unescape(template)
+    end
+
+    def url_helpers
+      Rails.application.routes.url_helpers
+    end
+  end
 end
---
+++ spec/zooplankton_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+require 'zooplankton'
+
+describe Zooplankton do
+  describe ".path_template_for" do
+    context "for a route with no required params" do
+      subject { Zooplankton.path_template_for(:root) }
+
+      it "returns just the path" do
+        expect(subject).to eq('/')
+      end
+    end
+
+    context "for a route with one required param" do
+      subject { Zooplankton.path_template_for(:post) }
+
+      it "uses simple string expansion to describe the parameter" do
+        expect(subject).to eq('/post/{slug}')
+      end
+    end
+
+    context "for a route with more than one required param" do
+      context "with no other arguments" do
+        subject { Zooplankton.path_template_for(:comment) }
+
+        it "uses simple string expansion to describe the parameters" do
+          expect(subject).to eq('/post/{slug}/comment/{comment_id}')
+        end
+      end
+
+      context "with one supplied parameter value" do
+        subject { Zooplankton.path_template_for(:comment, slug: 'post-slug') }
+
+        it "uses simple string expansion to describe the parameters" do
+          expect(subject).to eq('/post/post-slug/comment/{comment_id}')
+        end
+      end
+    end
+  end
+end
