98dc2e2ba3e3281ad9a124c55eb63deb4d0332c9
Ben Hamill <git-commits@benhamill.com>
2013-11-14 21:20:56 -0600

    Refactor for great justice.

--- lib/zooplankton.rb
+++ lib/zooplankton.rb
@@ -4,30 +4,36 @@ require "zooplankton/version"
 
 module Zooplankton
   class << self
-    def path_template_for(helper_name, query_params_or_required_params={}, required_params=nil)
-      return unless named_routes.names.include?(helper_name)
+    def path_template_for(helper_name, query_params={}, required_params=nil)
+      build_template(:path, helper_name, *parse_params(query_params, required_params))
+    end
 
-      if query_params_or_required_params.respond_to?(:to_h)
+    def url_template_for(helper_name, query_params={}, required_params=nil)
+      build_template(:url, helper_name, *parse_params(query_params, required_params))
+    end
+
+    private
+
+    def parse_params(*args)
+      if args.first.respond_to?(:to_h)
         query_params = []
-        required_params = query_params_or_required_params.to_h
+        required_params = args.first.to_h
       else
-        query_params = Array(query_params_or_required_params)
-        required_params ||= {}
+        query_params = Array(args.shift)
+        required_params = args.first || {}
       end
 
-      escaped_template_without_query_params = expand_helper(helper_name, :path, required_params)
+      [query_params, required_params]
+    end
+
+    def build_template(type, helper_name, query_params, required_params)
+      return unless named_routes.names.include?(helper_name)
+
+      escaped_template_without_query_params = expand_helper(helper_name, type, required_params)
       escaped_template = append_query_params(escaped_template_without_query_params, query_params)
 
       unescape_template(escaped_template)
     end
-
-    def url_template_for(helper_name, params={})
-      return unless named_routes.names.include?(helper_name)
-
-      unescape_template(expand_helper(helper_name, :url, params))
-    end
-
-    private
 
     def append_query_params(template, query_params)
       return template unless query_params.any?
