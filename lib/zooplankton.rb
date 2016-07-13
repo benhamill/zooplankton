939c464e625aa7ffa09763bc6936e97380f4e512
Ismael Celis <ismaelct@gmail.com>
2016-07-13 15:24:41 +0100

    Eagerly instantiate Parser to avoid threading issues.

--- lib/zooplankton.rb
+++ lib/zooplankton.rb
@@ -3,18 +3,14 @@ require "zooplankton/parser"
 
 module Zooplankton
   class << self
+    @@parser = Parser.new
+
     def path_template_for(helper_name, query_params = {}, supplied_params = nil)
-      parser.path_template_for(helper_name, query_params, supplied_params)
+      @@parser.path_template_for(helper_name, query_params, supplied_params)
     end
 
     def url_template_for(helper_name, query_params = {}, supplied_params = nil)
-      parser.url_template_for(helper_name, query_params, supplied_params)
-    end
-
-    private
-
-    def parser
-      @parser ||= Parser.new
+      @@parser.url_template_for(helper_name, query_params, supplied_params)
     end
   end
 end
